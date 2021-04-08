0fdf135de5e15433bfde8fb878604c88a3c4182d
William <xanthus1@gmail.com>
2021-04-07 20:15:43 -0400

    Added Todo Delete API Endpoint

--- server.js
+++ server.js
@@ -38,22 +38,34 @@ todoRoutes.route('/:id').get(function(req, res) {
   });
 });
 
+todoRoutes.route('/delete/:id').delete(function(req, res) {
+  let id = req.params.id;
+  console.log("deleting id: "+id);
+  Todo.deleteOne({ _id:id}, function(err, todo) {
+    if (!todo)
+      res.status(404).send("data is not found");
+    else
+      res.json(todo);
+  });
+});
+
 todoRoutes.route('/update/:id').post(function(req, res) {
   Todo.findById(req.params.id, function(err, todo) {
-  if (!todo)
-    res.status(404).send("data is not found");
-  else
-    todo.todo_description = req.body.todo_description;
-    todo.todo_responsible = req.body.todo_responsible;
-    todo.todo_priority = req.body.todo_priority;
-    todo.todo_completed = req.body.todo_completed;
+    if(!todo)
+      res.status(404).send("data is not found");
+    else{
+      todo.todo_description = req.body.todo_description;
+      todo.todo_responsible = req.body.todo_responsible;
+      todo.todo_priority = req.body.todo_priority;
+      todo.todo_completed = req.body.todo_completed;
 
-    todo.save().then(todo => {
-      res.json('Todo updated!');
-    })
-    .catch(err => {
-      res.status(400).send("Update not possible");
-    });
+      todo.save().then(todo => {
+        res.json('Todo updated!');
+      })
+      .catch(err => {
+        res.status(400).send("Update not possible");
+      });
+    }
   });
 });
 
